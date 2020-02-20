71e2df4c2f4543f209af3948ac384c02687fd78b
azusamatayoshi <44611159+azusamatayoshi@users.noreply.github.com>
2020-02-20 09:45:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let myGamePiece;
+
 function startGame() {
   myGameArea.start();
+  myGamePiece = new component(30, 30, "red", 10, 120);
 }
 
 let myGameArea = {
@@ -10,4 +13,14 @@ let myGameArea = {
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
   }
+}
+
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  ctx = myGameArea.context;
+  ctx.fillStyle = color;
+  ctx.fillRect(this.x, this.y, this.width, this.height);
 }
