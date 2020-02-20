c7f376288f84a62024946532b28b18d773e1aa20
azusamatayoshi <44611159+azusamatayoshi@users.noreply.github.com>
2020-02-20 11:59:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function component(width, height, color, x, y) {
   this.height = height;
   this.x = x;
   this.y = y;
-  this.update() = function() {
+  this.update = function() {
     ctx = myGameArea.context;
     ctx.fillStyle = color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
