883b356e1290e6165f14d5e821f7d6be063b717b
azusamatayoshi <44611159+azusamatayoshi@users.noreply.github.com>
2020-02-20 11:19:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ let myGameArea = {
     this.canvas.height = 270;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  clear : function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
 
@@ -20,7 +24,15 @@ function component(width, height, color, x, y) {
   this.height = height;
   this.x = x;
   this.y = y;
+  this.update() = function() {
   ctx = myGameArea.context;
   ctx.fillStyle = color;
   ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
 }
+
+function updateGameArea() {
+  myGameArea.clear();
+  myGamePiece.update();
+}
+  
