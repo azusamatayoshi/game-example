c2f2be793d9329cd48baa8d44f91999489dfe1bb
azusamatayoshi <44611159+azusamatayoshi@users.noreply.github.com>
2020-02-21 10:11:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let myGamePiece;
+let myObstacle;
 
 function startGame() {
   myGamePiece = new component(30, 30, "red", 10, 120);
+  myObstacle = new component(10, 200, "green", 300, 120);
   myGameArea.start();
 }
 
@@ -39,6 +41,7 @@ function component(width, height, color, x, y) {
 
 function updateGameArea() {
   myGameArea.clear();
+  myObstacle.update();
   myGamePiece.newPos();
   myGamePiece.update();
 }
