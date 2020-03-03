fb65d753e50512b22ef03cb01e4507a433347c9a
azusamatayoshi <44611159+azusamatayoshi@users.noreply.github.com>
2020-03-03 10:31:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let myGamePiece;
 let myObstacle [];
 
-function startGame() {
+function startGame() {/*
   myGamePiece = new component(30, 30, "red", 10, 120);
-  myObstacle = new component(10, 200, "green", 300, 120);
+  myObstacle = new component(10, 200, "green", 300, 120);*/
   myGameArea.start();
 }
 
